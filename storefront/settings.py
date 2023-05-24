cd85b6d323bb5e1b31a56dc8a63340407574d3ea
Kinsammy <fanusamuel@gmail.com>
2023-05-24 07:03:17 +0100

    CORS installed

--- storefront/settings.py
+++ storefront/settings.py
@@ -43,6 +43,13 @@ DJANGO_APPS = [
     'django_filters',
 ]
 
+THIRD_PARTY_APPS = [
+    'corsheaders',
+    'debug_toolbar',
+    'djoser',
+    'rest_framework',
+]
+
 LOCAL_APPS = [
     'store',
     'core',
@@ -51,15 +58,10 @@ LOCAL_APPS = [
     'playground',
 ]
 
-THIRD_PARTY_APPS = [
-    'debug_toolbar',
-    'rest_framework',
-    'djoser',
-]
-
 INSTALLED_APPS = DJANGO_APPS + LOCAL_APPS + THIRD_PARTY_APPS
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -149,6 +151,11 @@ INTERNAL_IPS = [
     # ...
     "127.0.0.1",
     # ...
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:8001',
+    'http://127.0.0.1:8001',
 ]
 
 REST_FRAMEWORK = {
