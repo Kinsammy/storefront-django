3e7ef40bd9e0f70de898e732ed8efcb80785b8fd
Kinsammy <fanusamuel@gmail.com>
2023-06-06 06:57:41 +0100

    Sending email

--- playground/urls.py
+++ playground/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('hello/', views.say_hello)
+    path('hello/', views.say_hello),
+    path('customer/', views.send_mail_to_customer),
+    path('admin-mail/', views.send_mail_to_admins),
+    path('attached-file/', views.send_mail_with_file_attached),
 ]
--- playground/views.py
+++ playground/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.core.mail import send_mail, mail_admins, BadHeaderError, EmailMessage
 from django.shortcuts import render
 
 from store.models import Product
@@ -9,4 +9,33 @@ from store.models import Product
 def say_hello(request):
     queryset = Product.objects.filter(inventory__lt=10).filter(unit_price__lt=20)
 
+
     return render(request, 'hello.html', {'name': 'Samuel', 'products': list(queryset)})
+
+
+def send_mail_to_customer(request):
+    try:
+        send_mail('subject', 'message', 'info@sammybuy.com',['sam@gmail.com'])
+    except BadHeaderError:
+        pass
+    return render(request, 'hello.html', {'name': 'Samuel'})
+
+
+def send_mail_to_admins(request):
+    try:
+        mail_admins('subject', 'message', html_message='message')
+    except BadHeaderError:
+        pass
+    return render(request, 'hello.html', {'name': 'Admin'})
+
+
+
+
+def send_mail_with_file_attached(request):
+    try:
+        message = EmailMessage('subject', 'message', 'info@sammybuy.com',['sam@gmail.com'])
+        message.attach_file('playground/static/images/slattery.jpg')
+        message.send()
+    except BadHeaderError:
+        pass
+    return render(request, 'hello.html', {'name': 'Attached File'})
