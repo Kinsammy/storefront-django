5ee446abd5b26cfc0bd86c27b8bc082860c9c435
Kinsammy <fanusamuel@gmail.com>
2023-06-20 21:01:26 +0100

    Perfomance Testing

--- store/views.py
+++ store/views.py
@@ -40,22 +40,17 @@ class ProductViewSet(ModelViewSet):
 
 
 class CollectionViewSet(ModelViewSet):
-    queryset = Collection.objects.all()
+    queryset = Collection.objects.annotate(
+        products_count=Count('products')).all()
     serializer_class = CollectionSerializer
     permission_classes = [IsAdminOrReadOnly]
 
-    class CollectionViewSet(ModelViewSet):
-        queryset = Collection.objects.annotate(
-            products_count=Count('products')).all()
-        serializer_class = CollectionSerializer
+    def destroy(self, request, *args, **kwargs):
+        if Product.objects.filter(collection_id=kwargs['pk']):
+            return Response({'error': 'Collection cannot be deleted because it includes one or more products.'},
+                            status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        return super().destroy(request, *args, **kwargs)
 
-        def delete(self, request, pk):
-            collection = get_object_or_404(Collection, pk=pk)
-            if collection.products.count() > 0:
-                return Response({'error': 'Collection cannot be deleted because it includes one or more products.'},
-                                status=status.HTTP_405_METHOD_NOT_ALLOWED)
-            collection.delete()
-            return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 class ReviewViewSet(ModelViewSet):
